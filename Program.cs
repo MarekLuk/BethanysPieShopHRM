e1e0eb1f633580d2b2ef41ef6b95dff4bb663c6f
MarekL-MarekL <markandmark3@gmail.com>
2024-02-27 14:13:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            //int result = GetSum(a, b);
+
+            int res2 = 0;
+
+
+            //Console.WriteLine(result);
+
+foreach (var item in args)
+            {
+               Console.WriteLine(item);
+         }
+
+            for (int i = 0; i <10; i++)
+            {
+                res2 = i;
+            }
+        }
+
+
+        //private static int GetSum(int a, int b)
+        //{
+        //    Console.WriteLine("Returning the sum.");
+        //    return a + b;
+        //}
+
+    }
+}
